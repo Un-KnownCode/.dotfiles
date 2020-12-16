d9a789d1f58ea51b7cff158936fabbc1d33c5ca8
Un-KnownCode <1183063391@qq.com>
2020-12-16 22:36:44 +0800

    dotfiles update

---
+++ tag-scripts/my.scripts/dwm.autostart.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dwmblocks &
+
+~/my.scripts/mailsync &
+~/my.scripts/chk.packages.sh &
+
+# picom
+picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+
+# fcitx5
+fcitx5 &
+
+# albert
+albert &
+
+~/my.scripts/setWallpaper.sh &
